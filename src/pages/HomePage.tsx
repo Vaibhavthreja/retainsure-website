b2aca7a4565d3c166e06193d46871aa7aaa3f889
Vaibhavthreja <vaibhav@retainsure.com>
2025-12-12 17:32:33 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -62,7 +62,7 @@ function HomePage() {
               <div className="flex items-start space-x-3">
                 <CheckCircle className="w-5 h-5 sm:w-6 sm:h-6 mt-0.5 flex-shrink-0" style={{ color: '#039143' }} />
                 <div>
-                  <h3 className="font-semibold text-base sm:text-lg" style={{ color: '#022610' }}>Grow revenue with 2× more accurate churn and upsell predictions.</h3>
+                  <h3 className="font-semibold text-base sm:text-lg" style={{ color: '#022610' }}>Grow revenue with 2× more accurate predictions</h3>
                   
                 </div>
               </div>
